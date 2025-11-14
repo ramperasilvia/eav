5136dd9cc967543277026cbc925d70b466ce42d1
ramperasilvia <rampera.silvia@gmail.com>
2025-11-14 16:13:57 -0300

    e
    e

---
+++ planner_function_index.js
@@ -0,0 +1,65 @@
+    <script>
+	
+	// simple left click on map
+	function onRouteMapClick(e) {
+
+	// double left click on map
+	function onRouteMapDbClick(e) {
+
+	// add a coordinate to all lists and map
+	function addRouteCoord(x, y) {
+
+	// center map at this coord
+	function goToCoord(x, y) {
+
+	// verify if coord is on the route list
+	function isRouteCoord(x, y) {
+
+	// draw coords on both maps and fill the route list
+	function populateRoute() {
+
+	// fill select with the route coords list
+	function addToRouteSelect(x, y, i) {
+	// remove coord from route select
+
+	function removeFromRouteSelect(i) {
+
+	// fill the hidden input and then get url with the route list
+	function populateHiddenRL() {
+
+	// draw a white square on the coord
+	function setFocusOnCoord(x, y) {
+
+//////////////////////////////////////////
+
+	// simple left click on wp map
+	function onWaypointsMapClick(e) {
+
+	// double left click on wp map
+	function onWaypointsMapDbClick(e) {
+
+	// add a coordinate to wp list and both maps
+	function addWaypoint(x, y) {
+
+	// center map at this waypoint
+	function goToWaypoint(x, y) {
+
+	// check if it is a waypoint on the list
+	function isWaypoint(x, y) {
+
+	// draw coords on the both maps and fill the wp list
+	function populateWaypoints() {
+
+	// fill select with the route coords list
+	function addToWaypointsSelect(x, y, i) {
+
+	// remove coord from route select
+	function removeFromWaypointsSelect(i) {
+
+	// fill the hidden input and then get url with the route list
+	function populateHiddenWPL() {
+
+	// draw a white square on the coord
+	function setFocusOnWaypointsMap(x, y) {
+
+    </script>
